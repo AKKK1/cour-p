8d02a6c30c89f808ef23aacbe90b5aa814f19822
AKKK1 <akakichachava1@gmail.com>
2024-12-13 22:06:41 +0100

    COURSE_PAY

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       '@typescript-eslint/ban-ts-comment': 'warn',
       '@typescript-eslint/no-empty-object-type': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
+      "import/no-anonymous-default-export": "off",
       '@typescript-eslint/no-unused-vars': [
         'warn',
         {
---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react'
+
+export default function page(): ReactElement {
+  return (
+    <div>
+      {' '}
+      <h1 className="text-3xl text-blue-500 font-bold"> HELLO WORLD</h1>
+      <p className="text-xl text-gray-600">Welcome to the Next.js application!</p>
+    </div>
+  )
+}
